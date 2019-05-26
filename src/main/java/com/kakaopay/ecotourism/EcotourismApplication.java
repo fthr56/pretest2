70d82f2d407b5e96d7e964f27d2e96e462a4567f
hue9010 <hue9010@gmail.com>
2019-05-26 15:45:31 +0900

    init

---
+++ src/main/java/com/kakaopay/ecotourism/EcotourismApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.ecotourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotourismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcotourismApplication.class, args);
+    }
+
+}
